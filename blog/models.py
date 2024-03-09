de96cc93ab9a525feb4aecc0a8473ac33d411a60
Brayan-Roncancio <brayanroncancio@usantotomas.edu.co>
2024-03-08 19:44:26 -0500

    funcionara

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     author = models.ForeignKey(
-        'auth.User',
+        max_length=200,
         on_delete=models.CASCADE,
     )
     body = models.TextField()
